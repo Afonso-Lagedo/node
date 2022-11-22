86ce4d71b75435d2a2e617211f71b4a3a495e37c
Afonso-cmd <afonso.ur@gmail.com>
2022-11-22 19:52:18 -0300

    Insomnia para requisições

--- index.js
+++ index.js
@@ -3,31 +3,11 @@ const express = require('express');//para usar o express
 //Necessita de um servidor:
 const server = express();
 
-//GET JSON
+const cursos = ['Node JS', 'JavaScript', 'React', 'React Native'];//simulando dados salvos
 
-/*Exemplo
-Query Params = ?nome=NodeJS
-Route Params = /curso/2
-Request Body = { nomw: 'Nodejs', tipo: 'Backend' }
-*/
-
-
-/*
-Exemplo 1
-
-server.get('/curso', (req, res)=>{
-    const nome = req.query.nome;
-    
-    return res.json({curso: `Aprendendo ${nome}`});
+server.get('/curso/:index', (req, res)=>{ // :index simulando id
+    const {index} = req.params;
+    return res.json(cursos[index]);
 })
-
-*/
-
-server.get('/curso/:id', (req, res)=>{ // :id esperando um route params 
-    const id = req.params.id;
-    
-    return res.json({curso: `Curso ID: ${id}`});
-})
-
 
 server.listen(3000);//definindo porta para servidor
